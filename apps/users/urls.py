2a2e99931bf31c74accffd9d76a36fc0df064ebc
jiangjun0130 <1137829446@qq.com>
2018-03-22 00:21:46 +0800

    机构管理

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,12 +4,14 @@ __date__ = '2018/3/16 9:42 '
 
 from django.urls import path, re_path
 
-from .views import UserInfoView, UploadImageView
+from .views import UserInfoView, UploadImageView, UpdatePwdView
 
 
 app_name = 'user'
 urlpatterns = [
     # 用户信息
     path('info/', UserInfoView.as_view(), name='user_info'),
-    path('image_upload/', UploadImageView.as_view(), name='image_upload')
+    path('image_upload/', UploadImageView.as_view(), name='image_upload'),
+    # 用户个人中心修改密码
+    path('update/pwd',UpdatePwdView.as_view(), name='update_pwd')
 ]
